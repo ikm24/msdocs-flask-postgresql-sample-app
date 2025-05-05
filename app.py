1f4f48fa88402204622f3be349e40f59daf6a72d
ikm24 <123666711+ikm24@users.noreply.github.com>
2025-05-05 18:43:46 +0000

    Codigo adaptado a proyecto

--- app.py
+++ app.py
@@ -32,92 +32,59 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # The import must be done after db initialization due to circular import issue
-from models import Restaurant, Review
+from models import ImagenCloud
 
 @app.route('/', methods=['GET'])
 def index():
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()
-    return render_template('index.html', restaurants=restaurants)
-
-@app.route('/<int:id>', methods=['GET'])
-def details(id):
-    restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant == id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews)
+    print('Solicitud para la página principal')
+    imagenes = ImagenCloud.query.all()
+    return render_template('index.html', imagenes=imagenes)
 
 @app.route('/create', methods=['GET'])
-def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
+def crear_imagen():
+    print('Solicitud para crear nueva ImagenCloud')
+    return render_template('crear_imagen.html')
 
 @app.route('/add', methods=['POST'])
 @csrf.exempt
-def add_restaurant():
+def add_imagen():
     try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
+        usuario = request.values.get('usuario')
+        imagen = request.values.get('imagen')
+        tipo = request.values.get('tipo')
+        pixelesR = request.values.get('pixelesR')
+        pixelesG = request.values.get('pixelesG')
+        pixelesB = request.values.get('pixelesB')
     except (KeyError):
-        # Redisplay the question voting form.
-        return render_template('add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
+
+        return redirect('crear_imagen', {
+            'error_message': "Todos los campos son obligatorios",
         })
     else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        db.session.add(restaurant)
+        imagenCloud = ImagenCloud()
+        imagenCloud.usuario = usuario
+        imagenCloud.imagen = imagen
+        imagenCloud.tipo = tipo
+        imagenCloud.pixelesR = int(pixelesR)
+        imagenCloud.pixelesG = int(pixelesG)
+        imagenCloud.pixelesB = int(pixelesB)
+        imagenCloud.fecha = datetime.now()
+        db.session.add(imagenCloud)
         db.session.commit()
 
-        return redirect(url_for('details', id=restaurant.id))
-
-@app.route('/review/<int:id>', methods=['POST'])
+        return redirect(url_for('index'))
+    
+@app.route('/remove/<int:id>', methods=['POST'])
 @csrf.exempt
-def add_review(id):
-    try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')
-    except (KeyError):
-        #Redisplay the question voting form.
-        return render_template('add_review.html', {
-            'error_message': "Error adding review",
-        })
-    else:
-        review = Review()
-        review.restaurant = id
-        review.review_date = datetime.now()
-        review.user_name = user_name
-        review.rating = int(rating)
-        review.review_text = review_text
-        db.session.add(review)
-        db.session.commit()
+def remove_imagen(id):
+    
+    imagen_a_eliminar = ImagenCloud.query.get_or_404(id)
+    
+    db.session.delete(imagen_a_eliminar)
+    db.session.commit()
+    print('Imagen de id {} eliminada'.format(id))
 
-    return redirect(url_for('details', id=id))
-
-@app.context_processor
-def utility_processor():
-    def star_rating(id):
-        reviews = Review.query.where(Review.restaurant == id)
-
-        ratings = []
-        review_count = 0
-        for review in reviews:
-            ratings += [review.rating]
-            review_count += 1
-
-        avg_rating = sum(ratings) / len(ratings) if ratings else 0
-        stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
-
-    return dict(star_rating=star_rating)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
